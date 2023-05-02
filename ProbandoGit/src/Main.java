3c79449c161bf8016faf02744f6f3c04d0394459
MaxiCastillo123 <84975191+MaxiCastillo123@users.noreply.github.com>
2023-05-02 00:55:12 -0300

    Add files via upload

---
+++ ProbandoGit/src/Main.java
@@ -0,0 +1,11 @@
+
+public class Main {
+    public static void main(String[] args) {
+      int num = 5;
+      for(int i = 0; i<5 ;i++){
+          System.out.println("El valor de i es de : " + i);
+      }
+
+      Persona personita1= new Persona("MAxi",30);
+    }
+}
